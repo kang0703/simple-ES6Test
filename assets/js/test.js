4af50307f73385e6c03045003dd4c57edfd40133
강 창민 <cmkang@gchealthcare.com>
2019-09-02 15:54:47 +0900

    업로드

---
+++ assets/js/test.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var kcm = function kcm() {
+	console.log("ddd");
+};
+
+var kcm33 = function kcm33() {
+	console.log("ddd");
+};
---
+++ es6/test.js
@@ -0,0 +1,9 @@
+
+	let kcm = () => {
+		console.log("ddd");
+	}
+
+
+	let kcm33 = () => {
+		console.log("ddd");
+	}
