7deafda68d2b30cdf575ea0405c8e6abe931523a
강 창민 <cmkang@gchealthcare.com>
2019-09-03 10:38:21 +0900

    공부중

--- es6/test.js
+++ es6/test.js
@@ -1,9 +1,49 @@
+/*
+ * 변수 ( let, const, var )
+ */
+// 자바스크립트 호이스팅이란 사전적 의미로 끌어올리기 라는뜻
+// 자바스크립트에서 끌어올려지는 것은 변수이다.
+// 변수의 정의가 그 범위에 따라 선언과 할당으로 분리되는것 이다.
 
-let kcm = () => {
-	console.log("ddd");
-}
 
+// var snack = "콘칩";
 
-let kcm33 = () => {
-	console.log("ddd");
-}
+// function getFood(food) {
+// 	console.log(snack);
+// 	if(food) {
+// 		var snack = '감자칩';
+// 		return snack;
+// 	}
+// 	return snack;
+// }
+// getFood(false);
+//
+//
+// 위의 코드는 실제 아래와 같다.
+// var snack = "콘칩";
+
+// function getFood(food) {
+// 	var snack;
+// 	if(food) {
+// 		snack = "감자칩";
+// 		return snack;
+// 	}
+// 	return snack;
+// }
+// getFood(false);
+
+/*
+ *******************************************************************************
+ */
+
+// let snack = '콘칩';
+
+// function getFood(food) {
+// 	if(food) {
+// 		let snack = '감자칩';
+// 		return snack;
+// 	}
+// 	return snack;
+// }
+
+// console.log(getFood(false));
